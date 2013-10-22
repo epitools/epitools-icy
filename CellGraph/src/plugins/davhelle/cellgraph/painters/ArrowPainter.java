2007a3339b3b2ae9d12d5362e07c94e66563c784
Davide Heller <davide.heller@imls.uzh.ch>
2013-10-22 16:39:22 +0200

    Refactored ArrowPainter

--- CellGraph/src/plugins/davhelle/cellgraph/painters/ArrowPainter.java
+++ CellGraph/src/plugins/davhelle/cellgraph/painters/ArrowPainter.java
@@ -5,11 +5,6 @@
  *=========================================================================*/
 package plugins.davhelle.cellgraph.painters;
 
-import icy.canvas.IcyCanvas;
-import icy.main.Icy;
-import icy.painter.Overlay;
-import icy.sequence.Sequence;
-
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
@@ -39,43 +34,71 @@ public class ArrowPainter extends AbstractGraphPainter {
 		this.displacement = displacement;
 	}
 	
+		
 	@Override
 	public void paintFrame(Graphics2D g, FrameGraph frame_i) {
-		AffineTransform defaultTransform = g.getTransform();
-
+		
 		for(Node cell: frame_i.vertexSet()){
-			if(cell.getNext() != null){
-				g.rotate(0);
-				Point next_cell_center = cell.getNext().getCentroid();
-				LineSegment segment_to_successive_cell = 
-						new LineSegment(
-								cell.getCentroid().getCoordinate(),
-								next_cell_center.getCoordinate()
-						);
-				
-				double segment_angle = segment_to_successive_cell.angle() + Math.PI/2;
-				
-				Point2D tipCoordinate = new Point2D.Double(next_cell_center.getX(),next_cell_center.getY());
-				
-				
-				//choose background color according to displacement length
-				if(segment_to_successive_cell.getLength() > (double) displacement)
-					g.setColor(Color.white);
-				else
-					g.setColor(Color.green);
-				g.fill(cell.toShape());
-				
-				g.setStroke(new BasicStroke((float)0.5));
-				g.setColor(Color.red);
-				g.draw(writer.toShape(segment_to_successive_cell.toGeometry(factory)));
-					
-				Triangle tip = new Triangle(2);
-				
-				g.setStroke(new BasicStroke((float)0.2));
-				g.rotate(segment_angle,tipCoordinate.getX(),tipCoordinate.getY());
-				g.fill(tip.createPoint(tipCoordinate));
-				g.setTransform(defaultTransform);
-			}
+			
+			//skipping cells which haven't been tracked
+			if(!cell.hasNext())
+				continue;	
+
+			//create segment line to next
+			Point next_cell_center = cell.getNext().getCentroid();
+			LineSegment segment_to_successive_cell = 
+					new LineSegment(
+							cell.getCentroid().getCoordinate(),
+							next_cell_center.getCoordinate()
+							);
+
+			Point2D tipCoordinate = new Point2D.Double(next_cell_center.getX(),next_cell_center.getY());				
+
+			draw(g, cell, segment_to_successive_cell, tipCoordinate);
 		}
+	}
+
+
+	private void draw(Graphics2D g, Node cell,
+			LineSegment segment_to_successive_cell, 
+			Point2D tipCoordinate) {
+		
+		g.rotate(0);
+		drawBackground(g, cell, segment_to_successive_cell);		
+		drawRotatedArrow(g, segment_to_successive_cell, tipCoordinate);
+	}
+
+
+	private Color getSegmentBackgroundColor(LineSegment segment_to_successive_cell) {
+		//choose background color according to displacement length
+		if(segment_to_successive_cell.getLength() > (double) displacement)
+			return Color.white;
+		else
+			return Color.green;
+	}
+	
+	private void drawBackground(Graphics2D g, Node cell,
+			LineSegment segment_to_successive_cell) { 
+		g.setColor(getSegmentBackgroundColor(segment_to_successive_cell));
+		g.fill(cell.toShape());
+	}
+
+
+	private void drawRotatedArrow(Graphics2D g,
+			LineSegment segment_to_successive_cell, Point2D tipCoordinate) {
+		//draw a line segment
+		g.setStroke(new BasicStroke((float)0.5));
+		g.setColor(Color.red);
+		g.draw(writer.toShape(segment_to_successive_cell.toGeometry(factory)));
+			
+		//draw a tip
+		Triangle tip = new Triangle(2);
+		AffineTransform defaultTransform = g.getTransform();
+		
+		g.setStroke(new BasicStroke((float)0.2));
+		double segment_angle = segment_to_successive_cell.angle() + Math.PI/2;
+		g.rotate(segment_angle,tipCoordinate.getX(),tipCoordinate.getY());
+		g.fill(tip.createPoint(tipCoordinate));
+		g.setTransform(defaultTransform);
 	}
 }

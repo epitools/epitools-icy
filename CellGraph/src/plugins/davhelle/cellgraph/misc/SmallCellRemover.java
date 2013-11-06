dae904c480685e78941f342f8ef5c79893ba2a49
Davide Heller <davide.heller@imls.uzh.ch>
2013-11-06 16:01:25 +0100

    Refactored SmallCellRemover to allow for Single Frame action;

--- CellGraph/src/plugins/davhelle/cellgraph/misc/SmallCellRemover.java
+++ CellGraph/src/plugins/davhelle/cellgraph/misc/SmallCellRemover.java
@@ -42,83 +42,90 @@ public class SmallCellRemover {
 	 */
 	public int removeCellsBelow(double threshold){
 		
-		int count = 0;
+		int count_of_removed_cells = 0;
 		
 		for(int time_point=0; time_point < stGraph.size(); time_point++){
-			ArrayList<Node> small_cell_list = new ArrayList<Node>();
+			int no_of_removed_cells_in_frame_i = removeCellsOnFrame(time_point, threshold);
 			
-			for(Node cell:stGraph.getFrame(time_point).vertexSet())
-				if(cell.getGeometry().getArea() < threshold){
-					small_cell_list.add(cell);
-					
-					
-					/*
-					 * The following section enables the 
-					 * SmallCellRemover to reassign the 
-					 * space of the too small cell to 
-					 * the cell which the longest intersection
-					 */
-					if(ABSORB_SMALL_CELLS){
-						
-						//System.out.println(cell.getCentroid().toText());
-
-						//find out the longest intersection with neighboring cells
-						double max_length = Double.MIN_VALUE;
-						Node max_length_candidate = null;
-						Geometry small_cell_geom = cell.getGeometry();
-						for(Node neighbor: cell.getNeighbors()){
-							Geometry neighbor_intersection = small_cell_geom.intersection(neighbor.getGeometry());
-							
-							//System.out.println("\t joins with "+neighbor.getCentroid().toText());
-							
-							//use the getLength method to find the largest intersection
-							//area not suited as the intersection is most likely a linear segment
-							//TODO check actual object type
-							
-							//double intersection_area = neighbor_intersection.getArea();
-							double intersection_length = neighbor_intersection.getLength();
-							
-							//System.out.println("\t\t area:"+intersection_area);
-							//System.out.println("\t\t length:"+intersection_length);
-							
-							if( intersection_length > max_length){
-								max_length = intersection_length;
-								max_length_candidate = neighbor;
-							}
-						}
-
-						if(max_length_candidate != null){
-							
-							//System.out.println("\t\t Winner: "+max_length_candidate.getCentroid().toText());
-							
-							//neighbor gains all neighbors of to_small_cell not yet connected and so do those
-							for(Node neighbor: cell.getNeighbors())
-								if(neighbor != max_length_candidate && !max_length_candidate.getNeighbors().contains(neighbor)){
-									//System.out.println("\t\t\t Adding "+neighbor.getCentroid().toText()+" as neighbor");
-									stGraph.getFrame(time_point).addEdge(max_length_candidate, neighbor);
-								}
-
-
-							//max_area_candidate obtains union with to_small_cell as new area
-							max_length_candidate.setGeometry(max_length_candidate.getGeometry().union(small_cell_geom));
-
-						}
-					}
-				
-				}
-			
-			
-			
-			//TODO update Graph in terms of neighborhoods?
-			//TODO fuse lost cell with containing/nearby cell? => need criteria for best fusing candidate
-			
-			stGraph.getFrame(time_point).removeAllVertices(small_cell_list);
-			
-			count += small_cell_list.size();
+			count_of_removed_cells += no_of_removed_cells_in_frame_i;
 		}
 		
-		return count;
+		return count_of_removed_cells;
 		
+	}
+
+	public int removeCellsOnFrame(int time_point, double threshold) {
+		
+		ArrayList<Node> small_cell_list = new ArrayList<Node>();
+		
+		for(Node cell:stGraph.getFrame(time_point).vertexSet())
+			if(cell.getGeometry().getArea() < threshold){
+				small_cell_list.add(cell);
+				
+				
+				/*
+				 * The following section enables the 
+				 * SmallCellRemover to reassign the 
+				 * space of the too small cell to 
+				 * the cell which the longest intersection
+				 */
+				if(ABSORB_SMALL_CELLS){
+					
+					//System.out.println(cell.getCentroid().toText());
+
+					//find out the longest intersection with neighboring cells
+					double max_length = Double.MIN_VALUE;
+					Node max_length_candidate = null;
+					Geometry small_cell_geom = cell.getGeometry();
+					for(Node neighbor: cell.getNeighbors()){
+						Geometry neighbor_intersection = small_cell_geom.intersection(neighbor.getGeometry());
+						
+						//System.out.println("\t joins with "+neighbor.getCentroid().toText());
+						
+						//use the getLength method to find the largest intersection
+						//area not suited as the intersection is most likely a linear segment
+						//TODO check actual object type
+						
+						//double intersection_area = neighbor_intersection.getArea();
+						double intersection_length = neighbor_intersection.getLength();
+						
+						//System.out.println("\t\t area:"+intersection_area);
+						//System.out.println("\t\t length:"+intersection_length);
+						
+						if( intersection_length > max_length){
+							max_length = intersection_length;
+							max_length_candidate = neighbor;
+						}
+					}
+
+					if(max_length_candidate != null){
+						
+						//System.out.println("\t\t Winner: "+max_length_candidate.getCentroid().toText());
+						
+						//neighbor gains all neighbors of to_small_cell not yet connected and so do those
+						for(Node neighbor: cell.getNeighbors())
+							if(neighbor != max_length_candidate && !max_length_candidate.getNeighbors().contains(neighbor)){
+								//System.out.println("\t\t\t Adding "+neighbor.getCentroid().toText()+" as neighbor");
+								stGraph.getFrame(time_point).addEdge(max_length_candidate, neighbor);
+							}
+
+
+						//max_area_candidate obtains union with to_small_cell as new area
+						max_length_candidate.setGeometry(max_length_candidate.getGeometry().union(small_cell_geom));
+
+					}
+				}
+			
+			}
+		
+		
+		
+		//TODO update Graph in terms of neighborhoods?
+		//TODO fuse lost cell with containing/nearby cell? => need criteria for best fusing candidate
+		
+		stGraph.getFrame(time_point).removeAllVertices(small_cell_list);
+		
+		return small_cell_list.size();
 	}
 	
 	

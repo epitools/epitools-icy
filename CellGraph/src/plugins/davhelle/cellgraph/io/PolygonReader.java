a4fb99c2bcfd89db7a82a393808783f451418124
Davide Heller <davide.heller@imls.uzh.ch>
2013-11-06 17:13:11 +0100

    Added Header and description

--- CellGraph/src/plugins/davhelle/cellgraph/io/PolygonReader.java
+++ CellGraph/src/plugins/davhelle/cellgraph/io/PolygonReader.java
@@ -1,10 +1,29 @@
+/*=========================================================================
+ *
+ *  Copyright Basler Group, Institute of Molecular Life Sciences, UZH
+ *
+ *=========================================================================*/
 package plugins.davhelle.cellgraph.io;
 
 import java.util.ArrayList;
 import com.vividsolutions.jts.geom.Polygon;
 
+/**
+ * Interface for all classes that extract the 
+ * JTS polygon structure from a given input 
+ * file.
+ * 
+ * @author Davide Heller
+ */
 public interface PolygonReader {
 	
+	/**
+	 * Extracts the polygons from the file
+	 * indicated in the absolute path
+	 * 
+	 * @param file_name absolute path of the input file
+	 * @return collection of JTS polygons representing the input
+	 */
 	public ArrayList<Polygon> extractPolygons(String file_name);
 
 }

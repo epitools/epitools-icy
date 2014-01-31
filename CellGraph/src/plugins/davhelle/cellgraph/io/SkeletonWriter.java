6bea99e21f5ff2fcae2f770d47913bd335b80bfc
Davide Heller <davide.heller@imls.uzh.ch>
2014-01-31 12:55:40 +0100

    SkeletonWriter: Further constraint the cells to output to the non-boundary & tracked cells in the first frame.

--- CellGraph/src/plugins/davhelle/cellgraph/io/SkeletonWriter.java
+++ CellGraph/src/plugins/davhelle/cellgraph/io/SkeletonWriter.java
@@ -7,9 +7,12 @@ import icy.type.DataType;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import loci.formats.FormatException;
 import plugins.davhelle.cellgraph.graphs.SpatioTemporalGraph;
+import plugins.davhelle.cellgraph.nodes.Division;
 import plugins.davhelle.cellgraph.nodes.Node;
 
 import com.vividsolutions.jts.geom.Coordinate;
@@ -42,17 +45,37 @@ public class SkeletonWriter {
 						true, 
 						SegmentationProgram.SeedWater);
 		
+		Set<Node> tracked_cells = fully_tracked_cells();
+		
 		for(int time_point = 0; time_point < stGraph.size(); time_point++){
 			String current_file_name = skeleton_file_name_generator.getFileName(time_point);
 			
-			IcyBufferedImage skeleton_img = createSkeleton(time_point);
+			IcyBufferedImage skeleton_img = createSkeleton(time_point, tracked_cells);
 			
 			saveSkeleton(current_file_name, skeleton_img);
 		}
 		
 	}
 	
-	private IcyBufferedImage createSkeleton(int t) {
+	private Set<Node> fully_tracked_cells(){
+		
+		HashSet<Node> tracked_cells = new HashSet<Node>();
+		for(Node cell: stGraph.getFrame(0).vertexSet()){
+			if(cell.getTrackID() != -1)
+				if(!cell.onBoundary()){
+					tracked_cells.add(cell);
+					if(cell.hasObservedDivision()){
+						Division d = cell.getDivision();
+						tracked_cells.add(d.getChild1());
+						tracked_cells.add(d.getChild2());
+					}
+				}		
+		}
+		
+		return tracked_cells;
+	}
+	
+	private IcyBufferedImage createSkeleton(int t, Set<Node> tracked_cells) {
 		
 		int img_width = sequence.getSizeX();
 		int img_height = sequence.getSizeY();
@@ -61,7 +84,7 @@ public class SkeletonWriter {
 		byte[] dataBuffer = img.getDataXYAsByte(0);
 
 		for(Node cell: stGraph.getFrame(t).vertexSet()){
-			if(cell.hasNext() || cell.hasPrevious()){
+			if(tracked_cells.contains(cell.getFirst())){
 				Geometry polygon = cell.getGeometry();
 				for(Coordinate vertex: polygon.getCoordinates()){
 

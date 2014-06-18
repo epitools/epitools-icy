02cac59e90f0b65e804cfaa1e13677df6495e9a5
Davide Heller <davide.heller@imls.uzh.ch>
2014-06-18 08:44:17 +0200

    Applied successfully tracking to the example

--- CellGraph/src/headless/T1Transitions.java
+++ CellGraph/src/headless/T1Transitions.java
@@ -11,6 +11,8 @@ import plugins.davhelle.cellgraph.graphs.SpatioTemporalGraphGenerator;
 import plugins.davhelle.cellgraph.misc.PolygonalCellTile;
 import plugins.davhelle.cellgraph.nodes.Node;
 import plugins.davhelle.cellgraph.painters.PolygonConverterPainter;
+import plugins.davhelle.cellgraph.tracking.NearestNeighborTracking;
+import plugins.davhelle.cellgraph.tracking.TrackingAlgorithm;
 
 /**
  * 
@@ -25,9 +27,9 @@ public class T1Transitions {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		  File test_file = new File("/Users/davide/tmp/T1/test/test_t0000.tif");
-		  //File test_file = new File("/Users/davide/tmp/T1/test2/test2_t0000.tif");
-		  int no_of_test_files = 1;
+		  //File test_file = new File("/Users/davide/tmp/T1/test/test_t0000.tif");
+		  File test_file = new File("/Users/davide/tmp/T1/test2/test2_t0000.tif");
+		  int no_of_test_files = 3;
 		  
 		  SpatioTemporalGraph stGraph = 
 				  new SpatioTemporalGraphGenerator(
@@ -37,11 +39,13 @@ public class T1Transitions {
 		  
 		 assert stGraph.size() == no_of_test_files: "wrong frame no";
 		 
+		 TrackingAlgorithm tracking_method = new NearestNeighborTracking(stGraph, 1, 5.0,1.0);
+		 tracking_method.track();
+		 
 		 for(int i=0; i < no_of_test_files; i++){
 			 System.out.printf("Anlalyzing frame %d\n", i);
-			 int cell_no = 0;
 			 for(Node n: stGraph.getFrame(i).vertexSet()){
-				 System.out.printf("Cell %d - ",cell_no++);
+				 System.out.printf("Cell %d - ",n.getTrackID());
 				 PolygonalCellTile tile = new PolygonalCellTile(n);
 				 System.out.printf("\tFound %d intersection/s\n",tile.getTileIntersectionNo());
 			 }

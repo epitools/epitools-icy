443b1741237570022019883c3bda0582bc6e9c4b
Davide Heller <davide.heller@imls.uzh.ch>
2014-09-18 13:12:31 +0200

    WKT: speed comparison

--- CellGraph/test/plugins/davhelle/cellgraph/io/WktPolygonExporterTest.java
+++ CellGraph/test/plugins/davhelle/cellgraph/io/WktPolygonExporterTest.java
@@ -13,21 +13,26 @@ public class WktPolygonExporterTest {
   @Test
   public void testSimpleWTKoutput() {
 	  
-	  	File test_file = new File("/Users/davide/data/neo/1/crop/skeletons_crop_t28-68_t0000.tif");
-		int no_of_test_files = 1;
-		
-		Assert.assertTrue(test_file.exists(),"Input File does not exist");
-		
-		SpatioTemporalGraphGenerator graphGenerator = 
-				new SpatioTemporalGraphGenerator(
-						GraphType.TISSUE_EVOLUTION,
-						test_file, no_of_test_files);
-		
-		SpatioTemporalGraph stGraph = graphGenerator.getStGraph();
+	  	SpatioTemporalGraph stGraph = loadTestGraph();
 		String frame_file_name = "/Users/davide/tmp/wkt_export/output.txt";
 		
 		new WktPolygonExporter(stGraph,frame_file_name);
 		
 		Assert.assertTrue(new File(frame_file_name).exists(),"Output File does not exist");
+  }
+
+  public static SpatioTemporalGraph loadTestGraph() {
+	  File test_file = new File("/Users/davide/data/neo/1/crop/skeletons_crop_t28-68_t0000.tif");
+	  int no_of_test_files = 1;
+
+	  Assert.assertTrue(test_file.exists(),"Input File does not exist");
+
+	  SpatioTemporalGraphGenerator graphGenerator = 
+			  new SpatioTemporalGraphGenerator(
+					  GraphType.TISSUE_EVOLUTION,
+					  test_file, no_of_test_files);
+
+	  SpatioTemporalGraph stGraph = graphGenerator.getStGraph();
+	  return stGraph;
   }
 }

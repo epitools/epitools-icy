4f298b6bc09009d8f010eca96c5388d16df0b3e1
Davide Heller <davide.heller@imls.uzh.ch>
2014-09-20 17:22:05 +0200

    Border Import: implemented first version with test and comparison.

--- CellGraph/src/plugins/davhelle/cellgraph/io/WktPolygonImporter.java
+++ CellGraph/src/plugins/davhelle/cellgraph/io/WktPolygonImporter.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKTFileReader;
@@ -31,6 +32,26 @@ public class WktPolygonImporter implements PolygonReader{
 		reader = new WKTReader();
 		
 	}
+	
+	public ArrayList<Geometry> extractGeometries(String file_name){
+		ArrayList<Geometry> stored_geometries = null;
+		
+		File file = new File(file_name);
+		WKTFileReader file_reader = new WKTFileReader(file, reader);
+		
+		try {
+			stored_geometries = (ArrayList<Geometry>) file_reader.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Something went wrong with the WKT reading");
+		}
+		
+		return stored_geometries;
+	}
+	
 	
 	@Override
 	public ArrayList<Polygon> extractPolygons(String file_name) {
--- CellGraph/test/plugins/davhelle/cellgraph/io/TestBorderExport.java
+++ CellGraph/test/plugins/davhelle/cellgraph/io/TestBorderExport.java
@@ -1,10 +1,12 @@
 package plugins.davhelle.cellgraph.io;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import plugins.davhelle.cellgraph.graphs.FrameGraph;
 import plugins.davhelle.cellgraph.graphs.GraphType;
 import plugins.davhelle.cellgraph.graphs.SpatioTemporalGraph;
 import plugins.davhelle.cellgraph.graphs.SpatioTemporalGraphGenerator;
@@ -18,17 +20,12 @@ public class TestBorderExport {
 	@Test
 	public void testBorderExport() {
 
-		File test_file = new File("/Users/davide/data/neo/1/crop/skeletons_crop_t28-68_t0000.tif");
-		int test_file_no = 4;
-			
-		SpatioTemporalGraph stGraph = 
-				new SpatioTemporalGraphGenerator(
-						GraphType.TISSUE_EVOLUTION,
-						test_file, 
-						test_file_no).getStGraph();
+		SpatioTemporalGraph stGraph = loadTestStGraph();
 
-		System.out.println("Identifying the border..");
+		long startTime = System.currentTimeMillis();
 		Geometry boundaries[] = new BorderCells(stGraph).markOnly();
+		long boundaryTime = System.currentTimeMillis() - startTime;
+		System.out.printf("Identifying the border took: %d ms\n",boundaryTime);
 		
 		//Test ouptput folder
 		String test_folder = "/Users/davide/tmp/wkt_export/";
@@ -41,6 +38,48 @@ public class TestBorderExport {
 			String expected_wkt_file = String.format("%sboundary%d.wkt",test_folder,i);
 			Assert.assertTrue(new File(expected_wkt_file).exists());
 		}
+	}
 
+
+	public SpatioTemporalGraph loadTestStGraph() {
+		File test_file = new File("/Users/davide/data/neo/1/crop/skeletons_crop_t28-68_t0000.tif");
+		int test_file_no = 4;
+			
+		SpatioTemporalGraph stGraph = 
+				new SpatioTemporalGraphGenerator(
+						GraphType.TISSUE_EVOLUTION,
+						test_file, 
+						test_file_no).getStGraph();
+		return stGraph;
+	}
+	
+	
+	@Test
+	public void testBorderImport(){
+		SpatioTemporalGraph stGraph = loadTestStGraph();
+		
+		//Test ouptput folder
+		String test_folder = "/Users/davide/tmp/wkt_export/";
+		
+		WktPolygonImporter importer = new WktPolygonImporter();
+		BorderCells border = new BorderCells(stGraph);
+		
+		long startTime = System.currentTimeMillis();
+		
+		for(int i=0; i<stGraph.size(); i++){
+			
+			String expected_wkt_file = String.format("%sboundary%d.wkt",test_folder,i);
+			Assert.assertTrue(new File(expected_wkt_file).exists());
+			
+			ArrayList<Geometry> boundaries = importer.extractGeometries(expected_wkt_file);
+			Assert.assertEquals(boundaries.size(), 1);
+			
+			FrameGraph frame = stGraph.getFrame(i);
+			border.markBorderCells(frame, boundaries.get(0));
+			
+		}
+		
+		long boundaryTime = System.currentTimeMillis() - startTime;		
+		System.out.printf("Loading the border took: %d ms\n",boundaryTime);
 	}
 }

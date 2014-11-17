8b58119d2385c7ed90dd4b63d1bb2d78d11097dc
Davide Heller <davide.heller@imls.uzh.ch>
2014-11-17 15:16:40 +0100

    Fast loading for neo samples

---
+++ CellGraph/src/plugins/davhelle/cellgraph/NeoLoader.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package plugins.davhelle.cellgraph;
+
+import headless.LoadNeoWtkFiles;
+import icy.gui.frame.progress.AnnounceFrame;
+import icy.main.Icy;
+import icy.painter.Overlay;
+import icy.sequence.Sequence;
+import icy.swimmingPool.SwimmingObject;
+import plugins.adufour.ezplug.EzPlug;
+import plugins.adufour.ezplug.EzVarInteger;
+import plugins.adufour.ezplug.EzVarSequence;
+import plugins.davhelle.cellgraph.graphs.SpatioTemporalGraph;
+import plugins.davhelle.cellgraph.painters.TrackIdPainter;
+import plugins.davhelle.cellgraph.painters.TrackPainter;
+
+/**
+ * Loads neo samples using wkt
+ * 
+ * @author Davide Heller
+ *
+ */
+public class NeoLoader extends EzPlug {
+
+	private EzVarSequence varSequence;
+	private EzVarInteger varNeo;
+
+	@Override
+	public void clean() {
+		
+	}
+
+	@Override
+	protected void execute() {
+		Sequence sequence = varSequence.getValue();
+		if(sequence == null){
+			new AnnounceFrame("Plugin requires active sequence! Please open an image on which to display results");
+			return;
+		}
+		
+		//Load stGraph
+		SpatioTemporalGraph wing_disc_movie = LoadNeoWtkFiles.loadNeo(varNeo.getValue());
+		
+		//Overlay tracking view
+		Overlay trackID = new TrackIdPainter(wing_disc_movie);
+		sequence.addOverlay(trackID);
+		Overlay correspondence = new TrackPainter(wing_disc_movie,true);
+		sequence.addOverlay(correspondence);
+		
+		//Push to swimming pool TODO: ONLY REMOVE stGraphs
+		Icy.getMainInterface().getSwimmingPool().removeAll();
+		SwimmingObject swimmingObject = new SwimmingObject(wing_disc_movie,"stGraph");
+		Icy.getMainInterface().getSwimmingPool().add( swimmingObject );
+	}
+
+	@Override
+	protected void initialize() {
+		
+		varSequence = new EzVarSequence("Input sequence");
+		super.addEzComponent(varSequence);
+		
+		varNeo = new EzVarInteger("Neo sample to load",0,1,2);
+		super.addEzComponent(varNeo);
+		
+	}
+
+}

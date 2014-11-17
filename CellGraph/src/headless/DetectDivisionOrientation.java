0b5027e2a7df906f3ace7d474921459e56414ac7
Davide Heller <davide.heller@imls.uzh.ch>
2014-11-17 17:26:12 +0100

    Externalized Orientation function

--- CellGraph/src/headless/DetectDivisionOrientation.java
+++ CellGraph/src/headless/DetectDivisionOrientation.java
@@ -8,6 +8,7 @@ import java.util.Iterator;
 import com.vividsolutions.jts.algorithm.Angle;
 import com.vividsolutions.jts.algorithm.MinimumBoundingCircle;
 import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
 
 import plugins.davhelle.cellgraph.graphs.SpatioTemporalGraph;
 import plugins.davhelle.cellgraph.graphs.FrameGraph;
@@ -35,19 +36,41 @@ public class DetectDivisionOrientation {
 				Division d = divisions.next();
 				Node mother = d.getMother();
 				
-				MinimumBoundingCircle mbc = new MinimumBoundingCircle(mother.getGeometry());
-				
-				Coordinate[] longest_axis = mbc.getExtremalPoints();
-				assert longest_axis.length == 2: "More than two coordinates!";
-				
-				double longest_axis_angle_rad = Angle.angle(longest_axis[0], longest_axis[1]);
-				double longest_axis_angle_deg = Angle.toDegrees(longest_axis_angle_rad);
+				Geometry mother_geomerty = mother.getGeometry();
+				double longest_axis_angle_rad = findAngleOfLongestAxis(mother_geomerty);
 				longest_axis_angle_rad = Math.abs(longest_axis_angle_rad);
 				
+				//Get children axis
+				Node child1 = d.getChild1();
+				Node child2 = d.getChild2();
 				
+				//TODO: possibly substitute here with edge (saved geo)
+				Geometry child_intersection = child1.getGeometry().intersection(child2.getGeometry());
+				double new_junction_angle = findAngleOfLongestAxis(child_intersection);
+				
+				double angle_difference = Angle.diff(new_junction_angle, longest_axis_angle_rad);
+				System.out.printf(
+						"Angle difference for dividing cell %d: %.2f\n",
+						mother.getTrackID(),
+						Angle.toDegrees(angle_difference));
 			}
 		}
 
+	}
+
+	/**
+	 * @param geometry_to_measure
+	 * @return
+	 */
+	private static double findAngleOfLongestAxis(Geometry geometry_to_measure) {
+		MinimumBoundingCircle mbc = new MinimumBoundingCircle(geometry_to_measure);
+		
+		Coordinate[] longest_axis = mbc.getExtremalPoints();
+		assert longest_axis.length == 2: "More than two coordinates!";
+		
+		double longest_axis_angle_rad = Angle.angle(longest_axis[0], longest_axis[1]);
+		double longest_axis_angle_deg = Angle.toDegrees(longest_axis_angle_rad);
+		return longest_axis_angle_rad;
 	}
 
 }

dcfaad03e8e98c90b056089d819a1a9bac316c79
Davide Heller <davide.heller@imls.uzh.ch>
2015-03-23 18:00:18 +0100

    PolygonPainter Extended to StGraphOverlay

--- CellGraph/src/plugins/davhelle/cellgraph/painters/PolygonPainter.java
+++ CellGraph/src/plugins/davhelle/cellgraph/painters/PolygonPainter.java
@@ -7,6 +7,10 @@ package plugins.davhelle.cellgraph.painters;
 
 import java.awt.Graphics2D;
 import java.awt.Color;
+
+import com.vividsolutions.jts.io.WKTWriter;
+
+import jxl.write.WritableSheet;
 import plugins.davhelle.cellgraph.graphs.FrameGraph;
 import plugins.davhelle.cellgraph.graphs.SpatioTemporalGraph;
 import plugins.davhelle.cellgraph.nodes.Node;
@@ -16,6 +20,8 @@ import icy.main.Icy;
 import icy.painter.AbstractPainter;
 import icy.painter.Overlay;
 import icy.sequence.Sequence;
+import icy.util.XLSUtil;
+import ij.process.EllipseFitter;
 
 /**
  * PolygonPainter depicts Polygons detected by JTS Polygonizer
@@ -23,37 +29,21 @@ import icy.sequence.Sequence;
  * @author Davide Heller
  *
  */
-public class PolygonPainter extends Overlay{
+public class PolygonPainter extends StGraphOverlay{
 	
-	private SpatioTemporalGraph stGraph;
+	public static final String DESCRIPTION = 
+			"Simple Overlay to show cells and their outlines in a color of choice";
+	
 	private Color painter_color;
 	
 	public PolygonPainter(SpatioTemporalGraph spatioTemporalGraph,Color painter_color){
-		super("Polygons");
-		this.stGraph = spatioTemporalGraph;
+		super("Polygons",spatioTemporalGraph);
 		this.painter_color = painter_color;
 	}
 
-	
 	@Override
-    public void paint(Graphics2D g, Sequence sequence, IcyCanvas canvas)
-    {
-		int time_point = Icy.getMainInterface().getFirstViewer(sequence).getPositionT();
-
-		if(time_point < stGraph.size()){
-			//TODO include 3D information (in case of VTK)!
-			paintFrame(g, time_point);
-			
-		}
-    }
-
-
-	/**
-	 * @param g
-	 * @param time_point
-	 */
-	public void paintFrame(Graphics2D g, int time_point) {
-		FrameGraph frame_i = stGraph.getFrame(time_point);
+	public void paintFrame(Graphics2D g, FrameGraph frame_i) {
+		
 		Color old = g.getColor();
 		g.setColor(painter_color);
 
@@ -61,5 +51,31 @@ public class PolygonPainter extends Overlay{
 			g.draw((cell.toShape()));
 
 		g.setColor(old);
+		
 	}
+
+	@Override
+	void writeFrameSheet(WritableSheet sheet, FrameGraph frame) {
+		WKTWriter writer = new WKTWriter(2);
+		
+		XLSUtil.setCellString(sheet, 0, 0, "Cell id");
+		XLSUtil.setCellString(sheet, 1, 0, "Centroid x");
+		XLSUtil.setCellString(sheet, 2, 0, "Centroid y");
+		XLSUtil.setCellString(sheet, 3, 0, "WKT String");
+
+		int row_no = 1;
+
+		for(Node n: frame.vertexSet()){
+			
+				XLSUtil.setCellNumber(sheet, 0, row_no, n.getTrackID());
+				XLSUtil.setCellNumber(sheet, 1, row_no, n.getCentroid().getX());
+				XLSUtil.setCellNumber(sheet, 2, row_no, n.getCentroid().getY());
+				XLSUtil.setCellString(sheet, 3, row_no, writer.write(n.getGeometry()));
+				row_no++;
+
+		}
+		
+	}
+	
+	
 }
